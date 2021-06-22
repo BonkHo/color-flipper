4aede602698bc72126ddc3687ba8694bee9fd81a
John <John@DESKTOP-SRO1BKN>
2021-06-21 22:10:07 -0700

    6-21-2021 update

---
+++ script.js
@@ -0,0 +1,22 @@
+// Finds the background color of body and shows what color it is
+let body = document.querySelector('body');
+let bodyStyle = window.getComputedStyle(body);
+let bodyBackground = bodyStyle.getPropertyValue('background-color');
+document.getElementById("background-color").innerHTML = bodyBackground;
+
+// If the button is clicked on will execture buttonEventListener
+document.getElementById("color-flipper").addEventListener("click", buttonEventListener);
+
+// Creates an array of colors
+let colors = ['Aero Blue', 'Opal', 'Periwinkle', 'Heliotrope', 'Liver'];
+let hexColor = ['#C4E7D4', '#C4DACF', '#B9C0DA', '#998DA0', '#63585E'];
+
+// Gets a random integer in the range 0 to max
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function buttonEventListener() {
+    document.getElementById("background-color").innerHTML = "Hello World";
+}
